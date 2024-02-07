0f5f61a36fc85525b2761a5f1506c72e16b691f1
Airat <airat.valiullin@tum.de>
2024-02-07 22:07:25 +0100

    better datetime_from_now

--- utils.py
+++ utils.py
@@ -26,15 +26,14 @@ class StatsResult:
 
 def datetime_from_now(dt: datetime.datetime) -> str:
     """Returns a string saying how long ago the datetime object is."""
-    now = datetime.datetime.now()
-    diff = now - dt
-    years = diff.days // 365; diff -= datetime.timedelta(days=years*365)
-    months = diff.days // 30; diff -= datetime.timedelta(days=months*30)
-    days = diff.days; diff -= datetime.timedelta(days=days)
-    hours = diff.seconds // 3600; diff -= datetime.timedelta(hours=hours)
-    minutes = diff.seconds // 60; diff -= datetime.timedelta(minutes=minutes)
-    words = ['year', 'month', 'day', 'hour', 'minute']
-    values = [years, months, days, hours, minutes]
+    total_seconds = int((datetime.datetime.now() - dt).total_seconds())
+    years, remainder = divmod(total_seconds, 31536000)
+    months, remainder = divmod(remainder, 2628000)
+    days, remainder = divmod(remainder, 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    words = ['year', 'month', 'day', 'hour', 'minute', 'second']
+    values = [years, months, days, hours, minutes, seconds]
     res = ''
     for value, word in dropwhile(lambda x: x[0] == 0, zip(values, words)):
         res += f'{value} {word}{"s" if value > 1 else ""} '
